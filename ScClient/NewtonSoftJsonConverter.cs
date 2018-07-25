67be26acb28403cc1f12f56085a8a60ac9a94d95
sachin shinde <sachinshinde7676@gmail.com>
2018-07-26 01:35:48 +0530

    added casing strategy for serialization and deserialization using NewtonSoft converter

--- ScClient/NewtonSoftJsonConverter.cs
+++ ScClient/NewtonSoftJsonConverter.cs
@@ -1,17 +1,26 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace ScClient
 {
     public class NewtonSoftJsonConverter : IJsonConverter
     {
+        private readonly JsonSerializerSettings _jsonSerializationSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            }
+        };
+
         public string Serialize(object model)
         {
-            return JsonConvert.SerializeObject(model);
+            return JsonConvert.SerializeObject(model, _jsonSerializationSettings);
         }
 
         public T Deserialize<T>(string json)
         {
-            return JsonConvert.DeserializeObject<T>(json);
+            return JsonConvert.DeserializeObject<T>(json, _jsonSerializationSettings);
         }
     }
 }
